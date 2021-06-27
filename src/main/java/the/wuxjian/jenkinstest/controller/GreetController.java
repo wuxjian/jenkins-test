ea180f06ee9bb65a4ea545dbc349f97a71ee2e1d
wuxjian <wu_xjian@163.com>
2021-06-27 16:14:47 +0800

    添加GreetController

---
+++ src/main/java/the/wuxjian/jenkinstest/controller/GreetController.java
@@ -0,0 +1,13 @@
+package the.wuxjian.jenkinstest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetController {
+
+    @GetMapping("/greet")
+    public String greet() {
+        return "greet";
+    }
+}
