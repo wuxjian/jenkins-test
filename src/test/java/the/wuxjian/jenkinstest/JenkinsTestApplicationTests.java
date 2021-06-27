d9d37ef1b001bcbb4a77c5df9cb8274ea6d5b668
wuxjian <wu_xjian@163.com>
2021-06-27 16:10:39 +0800

    init

---
+++ src/test/java/the/wuxjian/jenkinstest/JenkinsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package the.wuxjian.jenkinstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
